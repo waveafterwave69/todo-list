09a401d85592575f01b98b2e0d10f1ce36a6ec01
Михаил <130276609+waveafterwave69@users.noreply.github.com>
2025-02-16 22:59:07 +0300

    first commit

---
+++ toDoList/js/main.js
@@ -0,0 +1,58 @@
+const editTitlteBtn = document.querySelector('.main-content-header-btn')
+const headerTitle = document.querySelector('.content-header-title')
+const addBtn = document.querySelector('.add-btn')
+const taskList = document.querySelector('.content-main-column')
+
+editTitlteBtn.addEventListener('click', function () {
+    let promptValue = prompt('Введите новое название.')
+    if (promptValue) {
+        if (promptValue.length < 15) {
+            headerTitle.textContent = promptValue
+            saveTitle()
+        } else {
+            alert('Слишком много текста!')
+        }
+    }
+})
+
+function saveTitle() {
+    localStorage.setItem('title', headerTitle.innerHTML)
+}
+
+function showTitle() {
+    headerTitle.innerHTML = localStorage.getItem('title')
+}
+
+showTitle()
+
+function addTask() {
+    const listItemValue = prompt('Введите дело')
+    if (listItemValue) {
+        let li = document.createElement('li')
+        li.innerHTML = listItemValue
+        taskList.appendChild(li)
+        let img = document.createElement('img')
+        img.src = './img/close.png'
+        li.appendChild(img)
+        saveData()
+    }
+}
+
+taskList.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked')
+        saveData()
+    } else if (event.target.tagName === 'IMG') {
+        event.target.parentElement.remove()
+        saveData()
+    }
+})
+
+function saveData() {
+    localStorage.setItem('data', taskList.innerHTML)
+}
+
+function showTask() {
+    taskList.innerHTML = localStorage.getItem('data')
+}
+showTask()
